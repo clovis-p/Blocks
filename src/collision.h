f8cdf0ed95bfd98c7e4a10fba3d21fcfbadebf17
Clovis <clovisperreault@gmail.com>
2023-04-24 22:38:14 -0400

    Fixed incorrect declaration

--- src/collision.h
+++ src/collision.h
@@ -4,7 +4,7 @@
 #define SET_COLLISION 0
 #define SET_INACTIVE 1
 
-int overlap();
+int overlap(gameObject o1, gameObject o2);
 void checkIfWithinBounds(gameObject *o, int flag);
 void checkEnemyCollisions(gameObject *enemy, gameObject player);
 
